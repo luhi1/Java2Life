1220690bdf6ebdf495e1aa7e7dbca574bb91bed4
luhi1 <100657351+luhi1@users.noreply.github.com>
2022-09-24 14:37:01 -0700

    Fixed the code (whoops)!

--- Unit_1/primitive_types_and_variables.java
+++ Unit_1/primitive_types_and_variables.java
@@ -1,19 +1,23 @@
-public class Test(){
+public class Test{
     public static void main(String[] args){
         //Whole Numbers
         int myNumber;
         myNumber = 300000;
         short smallNumber = 3000;
-        long massiveNumber = 3000000000;
+        long massiveNumber = 300000;
         
         //Decimal Points
-        float myFloat = 2.555;
+        float myFloat = 2;
+        //Floats are not really ever used, and I think CS Awesome does not like floats!
         double myDouble = 2.555555555555555;
         
         //One or Zero
         boolean myBoolean = true;
         
         //One Character (Alphabetical key on the keyboard or special character)
-        char myCharacter = "\u0095";
+        char myCharacter = '\u0095';
     }
 }
+
+
+                  
