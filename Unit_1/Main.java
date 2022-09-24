b6e4b3bac89597073a92c2ebd5761eb925efef1b
lulhi <michaelborov2007@gmail.com>
2022-09-23 17:08:09 -0700

    The Beggining!

---
+++ Unit_1/Main.java
@@ -0,0 +1,9 @@
+//Ignore the package name for now! Not important
+package Unit_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Human newHuman = new Human();
+        newHuman.setHairColor("Green");
+    }
+}
