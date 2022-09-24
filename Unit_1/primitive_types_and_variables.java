689c21ec496fc1cfe3cdb5a44494d2c157cd9b89
luhi1 <100657351+luhi1@users.noreply.github.com>
2022-09-23 20:58:27 -0700

    Unit 1: Primitives and Data Types Video Code

---
+++ Unit_1/primitive_types_and_variables.java
@@ -0,0 +1,19 @@
+public class Test(){
+    public static void main(String[] args){
+        //Whole Numbers
+        int myNumber;
+        myNumber = 300000;
+        short smallNumber = 3000;
+        long massiveNumber = 3000000000;
+        
+        //Decimal Points
+        float myFloat = 2.555;
+        double myDouble = 2.555555555555555;
+        
+        //One or Zero
+        boolean myBoolean = true;
+        
+        //One Character (Alphabetical key on the keyboard or special character)
+        char myCharacter = "\u0095";
+    }
+}
