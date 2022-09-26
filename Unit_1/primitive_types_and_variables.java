ae39a5218e7d4ca914687efb96a337020869de11
SovietDog <61475176+sovietdog64@users.noreply.github.com>
2022-09-26 17:33:45 +0000

    Update primitive_types_and_variables.java

--- Unit_1/primitive_types_and_variables.java
+++ Unit_1/primitive_types_and_variables.java
@@ -1,7 +1,7 @@
 public class Test{
     public static void main(String[] args){
         //Whole Numbers
-        int myNumber; // int types can store up to 4 bytes (32 bit, 8 bits per byte)
+        int myNumber; // int types can store up to 4 bytes (32 bits, 8 bits per byte)
         myNumber = 300000; // stores from -2,147,483,648 to 2,147,483,647
         short smallNumber = 3000; // 2 bytes
         long massiveNumber = 300000; // 8 bytes
@@ -9,7 +9,7 @@ public class Test{
         // knowing how much an int will need to store can make your program more memory efficient.
         
         //Decimal Points
-        float myFloat = 2; // 4 bytes
+        float myFloat = 2f; // 4 bytes
         //Floats are not really ever used, and I think CS Awesome does not like floats!
         double myDouble = 2.555555555555555; // 8 bytes
         
