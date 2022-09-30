e48846e356e4abf7410c48425306368200261c5c
luhi1 <michaelborov2007@gmail.com>
2022-09-29 17:38:44 -0700

    Added the code for the casting video.

---
+++ Unit_1/casting_and_errors_video.java
@@ -0,0 +1,31 @@
+public class Test{
+    public static void main(String[] args){
+        //Syntax, Runtime, and Exception Errors
+        //aasdfljkasdfkjasdlkfasdjflk
+        //int tooLong = 111111111111111111111111111;
+        //System.out.println(0/0);
+        
+        System.out.println(3 / 2);
+        // Int / Int = Int
+        System.out.println((double) 3 / 2);
+        System.out.println(3.0 / 2);
+        // Double / Int = Double (The highest in the hierarchy = the overall type most of the time (Strings are the exception, unit 2....) )
+
+        double decimalTrunk = 3.77;
+        int trunkInt = (int) decimalTrunk;
+
+        System.out.println(trunkInt);
+
+        //Countless examples
+        System.out.println(1.0 / 2);
+        System.out.println(1 / 2);
+        System.out.println(0.0);
+        System.out.println(1.5);
+
+        //System.out.println((String) 1);
+    }
+}
+
+
+
+                  
